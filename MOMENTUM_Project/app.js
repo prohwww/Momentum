5ab990bf4a7388af3be19d3d9bc7d0c8f0d74fde
prohwww <isk03214@gmail.com>
2022-03-08 21:32:23 +0900

    Day2 complete

--- MOMENTUM_Project/app.js
+++ MOMENTUM_Project/app.js
@@ -1,1 +1,12 @@
-alert("hi");
+const player = {
+    name: "nico",
+    points: 10,
+    fat: true,
+};
+
+console.log(player);
+player.fat = false;
+console.log(player.name);
+player.lastname = "potato";
+player.points = 15;
+console.log(player);
