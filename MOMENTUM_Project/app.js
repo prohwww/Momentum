a34b104b371847f16028109ae365a5205b465ee6
prohwww <isk03214@gmail.com>
2022-05-16 21:41:04 +0900

    update files

--- MOMENTUM_Project/app.js
+++ MOMENTUM_Project/app.js
@@ -1,15 +1,32 @@
-// const loginFrom = document.querySelector("#login-form");
-// const loginInput = loginFrom.querySelector("input");
-// const loginButton = loginFrom.querySelector("button");
-
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
-function onLoginSubmit(tomato) {
-  tomato.preventDefault();
-  console.log(tomato);
-  // const userName = loginInput.value;
-  // console.log(userName);
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+function onLoginSubmit(event) {
+  event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const inputUserName = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, inputUserName);
+  paintGreetings();
+}
+// handlerLickClick({information about the event that just happend})
+
+function paintGreetings(username) {
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+const savedUserName = localStorage.getItem(USERNAME_KEY);
+
+console.log(savedUserName);
+if (savedUserName === null) {
+  // show the form
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  // show the greetings
+  paintGreetings(savedUserName);
+}
