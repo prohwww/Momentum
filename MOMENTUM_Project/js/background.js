aaea3d741a76c74d88de45116d65f9a097fe6a76
prohwww <isk03214@gmail.com>
2022-05-19 23:25:30 +0900

    update files

---
+++ MOMENTUM_Project/js/background.js
@@ -0,0 +1,14 @@
+const images = [
+    "cat.jpg",
+    "nature.jpg",
+    "tree.jpg"
+];
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+// html의 element 생성
+const bgImage = document.createElement("img");
+
+bgImage.src = `img/${chosenImage}`;
+
+document.body.appendChild(bgImage);
